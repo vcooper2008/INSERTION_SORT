ad2e49665ef34619c70f3368740c0cf17495d4f8
vcooper <vanessa.cooper1@csuglobal.edu>
2024-08-22 11:47:03 -0500

    Added timeit

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,3 +1,5 @@
+import timeit
+
 def insertion_sort(arr):
     #Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
@@ -16,6 +18,17 @@ def insertion_sort(arr):
 
 if __name__ == "__main__":
     data = [12, 11, 13, 5, 6]
+
+    # Make a copy of the data for timeit to ensure it remains unchanged between runs
+    data_copy = data.copy()
+
+    #Measure the execution time using timeit
+    execution_time = timeit.timeit(stmt="insertion_sort(data_copy)",
+                                   setup="from __main__ import insertion_sort, data_copy",
+                                   number=1000
+                                   )
     print("Original array: ", data)
     insertion_sort(data)
     print("Sorted array: ", data)
+
+    print(f"Execution Time over 1000 runs: {execution_time:.6f}  seconds")
