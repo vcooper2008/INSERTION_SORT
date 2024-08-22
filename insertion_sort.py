15b6a2d88374ceb44e5cb11f26f826e92f38bd5c
vcooper <vanessa.cooper1@csuglobal.edu>
2024-08-22 12:41:04 -0500

    Updated for memory usage as well.

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,5 @@
 import timeit
+import tracemalloc
 
 def insertion_sort(arr):
     #Traverse through 1 to len(arr)
@@ -15,6 +16,25 @@ def insertion_sort(arr):
         # Place the key at the correct position
         arr[j + 1] = key
 
+# Function to measure execution time
+def measure_execution_time(sort_function, data, repetitions):
+    data_copy = data.copy()
+    statement = sort_function.__name__ + "(data_copy)"
+    set_up = "from __main__ import " + sort_function.__name__ +", data_copy"
+    execution_time = timeit.timeit(stmt=statement,
+                                   setup=set_up,
+                                   number=repetitions
+                                   )
+    return execution_time
+
+# Function to measure memory usage
+def measure_memory_usage(sort_function, data):
+    data_copy = data.copy()
+    tracemalloc.start()
+    sort_function(data_copy)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    return current, peak
 
 if __name__ == "__main__":
     data = [12, 11, 13, 5, 6]
@@ -23,12 +43,17 @@ if __name__ == "__main__":
     data_copy = data.copy()
 
     #Measure the execution time using timeit
-    execution_time = timeit.timeit(stmt="insertion_sort(data_copy)",
-                                   setup="from __main__ import insertion_sort, data_copy",
-                                   number=1000
-                                   )
+    repetitions = 1000
+    exec_time = measure_execution_time(insertion_sort, data, repetitions)
+
+    #Measure memory usage
+    current_memory, peak_memory = measure_memory_usage(insertion_sort, data)
+
+    # Print results
     print("Original array: ", data)
-    insertion_sort(data)
+    insertion_sort(data.copy())
     print("Sorted array: ", data)
 
-    print(f"Execution Time over 1000 runs: {execution_time:.6f}  seconds")
+    print(f"Execution Time over {repetitions} runs: {exec_time:.6f}  seconds")
+    print(f"Current Memory Usage: {current_memory / 10**6:.6f} MB")
+    print(f"Peak Memory Usage: {peak_memory / 10**6:.6f} MB")
